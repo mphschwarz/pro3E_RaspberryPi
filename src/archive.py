ec525be047c1435b4d665d57afbba587eb1a4a5b
Michael Schwarz <michael.schwarz@flarm.com>
2017-11-09 16:25:37 +0100

    added standard requests

--- src/archive.py
+++ src/archive.py
@@ -5,30 +5,26 @@ import time
 class DataPoint:
     """mean data over a measuring period"""
 
-    def __init__(self, volt, amp, real_power, imag_power, frequency):
+    def __init__(self, volt, amp, real_power, imag_power):
         """
         :param volt: rms value of voltage
         :param amp: rms value of current
-        :param frequency: voltage frequency
         """
         self.time_stamp = time.time()  # time stamp in unix-time
         self.voltage = volt
         self.currant = amp
-        self.frequency = frequency
         self.real_power = real_power
         self.imag_power = imag_power
 
     def __repr__(self):
-        return 't:{}; V:{}, I:{}; P:{}, Q:{}; f:{}'.format(self.time_stamp,
-                                                           self.voltage, self.currant,
-                                                           self.real_power, self.imag_power,
-                                                           self.frequency)
+        return 't:{}; V:{}, I:{}; P:{}, Q:{}'.format(self.time_stamp,
+                                                     self.voltage, self.currant,
+                                                     self.real_power, self.imag_power)
 
     def __str__(self):
-        return '{}; {}, {}; {}, {}; {}'.format(self.time_stamp,
-                                               self.voltage, self.currant,
-                                               self.real_power, self.imag_power,
-                                               self.frequency)
+        return '{}; {}, {}; {}, {}'.format(self.time_stamp,
+                                           self.voltage, self.currant,
+                                           self.real_power, self.imag_power)
 
 
 def make_text_file(file_path, data):
--- src/input.py
+++ src/input.py
@@ -1,13 +1,18 @@
 import serial
 import time
 
+import src.archive
+
+
 def init_devs(dev_name='/dev/ttyACM0', boud=115200, timeout=0.1):
     """:returns pointer to serial device"""
     return serial.Serial(dev_name, boud, timeout=timeout)
 
+
 def request_value(device, value):
-    """:param device: pointer to serial device
-    :param value: u, i, p, q, x for voltage, current, real- and imag power, x for test
+    """requests voltage or current or real power or imag power or dummy response over serial connection
+    :param device: pointer to serial device
+    :param value: u -> voltage, i -> current, p -> real power, q -> imag power, x -> test response
     :returns measured value"""
     device.write(bytes(value, 'utf-8'))
     time.sleep(0.001)
@@ -17,3 +22,10 @@ def request_value(device, value):
     else:
         return None
 
+
+def request_full_data(device):
+    """:returns a single Data Point"""
+    return src.archive.DataPoint(request_value(device, 'u'),
+                                 request_value(device, 'i'),
+                                 request_value(device, 'p'),
+                                 request_value(device, 'q'))
